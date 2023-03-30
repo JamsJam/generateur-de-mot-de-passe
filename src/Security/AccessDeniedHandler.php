c8e875b5f1516ba6ee37b88e2eea12fb52e4dbeb
Jams <Jamsjam971@hotmail.com>
2023-03-30 15:28:08 -0400

    error http

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException): ?Response
+    {
+        // log access denied in database
+        // code to log access denied in database
+
+        // create a flash message
+        $request->getSession()->getFlashBag()->add('note', 'You have to login in order to access this page.');
+
+        // redirect to the route /manage
+        $url = $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost() . $request->getBaseUrl() . '/manage';
+        return new Response('', 302, ['Location' => $url]);
+    }
+}
