25641a4936a2a2c4b138ce283d95859fb6438dbb
radjaho973 <adjaho.b@hotmail.fr>
2023-03-30 16:57:32 -0300

    resolution de conflits

--- src/Security/AuthenticationEntryPoint.php
+++ src/Security/AuthenticationEntryPoint.php
@@ -19,6 +19,6 @@ class AuthenticationEntryPoint implements AuthenticationEntryPointInterface
         // add a custom flash message and redirect to the login page
         $request->getSession()->getFlashBag()->add('Auth_error', 'You have to login in order to access this page.');
 
-        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        return new RedirectResponse($this->$urlGenerator->generate('app_login'));
     }
 }
