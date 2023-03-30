1707ee9049973ce1411f49bbf302021e6b766203
Jams <Jamsjam971@hotmail.com>
2023-03-30 18:24:52 -0400

    error http

--- src/Security/AuthenticationEntryPoint.php
+++ src/Security/AuthenticationEntryPoint.php
@@ -12,6 +12,7 @@ class AuthenticationEntryPoint implements AuthenticationEntryPointInterface
     public function __construct(
         UrlGeneratorInterface $urlGenerator,
     ) {
+        
     }
 
     public function start(Request $request, AuthenticationException $authException = null): RedirectResponse
@@ -19,6 +20,6 @@ class AuthenticationEntryPoint implements AuthenticationEntryPointInterface
         // add a custom flash message and redirect to the login page
         $request->getSession()->getFlashBag()->add('Auth_error', 'You have to login in order to access this page.');
 
-        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        return new RedirectResponse($this->$urlGenerator->generate('app_login'));
     }
 }
